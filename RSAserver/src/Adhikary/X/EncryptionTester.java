d24b25a2bc7fc345a54b92b1b2c8d95a760b3e6e
bishalraiganj <bishaladhikaryreal@gmail.com>
2025-06-07 18:58:51 +0530

    decryption logic error

--- RSAserver/src/Adhikary/X/EncryptionTester.java
+++ RSAserver/src/Adhikary/X/EncryptionTester.java
@@ -1,10 +1,28 @@
 package Adhikary.X;
 
+import java.math.BigInteger;
+
 public class EncryptionTester {
 
 	public static void main(String... args)
 	{
 
+		RsaCipher c1 = new RsaCipher();
+
+		String plainText = "Bishal Adhikary Testing RsaCipher class logic 07-06-25";
+
+		BigInteger eInt = c1.encrypt(plainText);
+
+		String cipherText = c1.encryptMessageFrmBi(eInt);
+
+		String originalText= c1.decryptString(cipherText);
+
+
+		System.out.println("encrypted BigInteger : " + c1.encrypt(plainText));
+
+		System.out.println("Cipher text (encrypted String) : " + cipherText );
+
+		System.out.println("Decrypted String (Original String / plaintext : " + originalText);
 
 
 
