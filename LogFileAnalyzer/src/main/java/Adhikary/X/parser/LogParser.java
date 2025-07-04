2761c8af1c8465dfdb207581654ec9cd6a67d4c5
Bishal Adhikary <158996175+bishalraiganj@users.noreply.github.com>
2025-07-05 01:25:37 +0530

    UNTESTED Regex

--- LogFileAnalyzer/src/main/java/Adhikary/X/parser/LogParser.java
+++ LogFileAnalyzer/src/main/java/Adhikary/X/parser/LogParser.java
@@ -1,6 +1,11 @@
 package Adhikary.X.parser;
 
+import java.util.regex.Pattern;
+
 public class LogParser {
+
+	// The pattern matches or finds in Log's of formats "YYYY-MM-DD HH:MM:SS LEVEL [Source] - Message" UNTESTED
+	private static final Pattern  pattern = Pattern.compile("(([1-9][0-9]{3})-([01][0-9])-([0123][0-9])) (([012][0-9]):([0-6][0-9]):([0-6][0-9])) ([A-Za-z]{3,40}) (\\[[a-zA-Z0-9]{3,300}\\]) - (([a-zA-Z0-9]{1,40}){1}([ ]{1}[a-zA-Z0-9]{1,40})*)");
 
 
 
