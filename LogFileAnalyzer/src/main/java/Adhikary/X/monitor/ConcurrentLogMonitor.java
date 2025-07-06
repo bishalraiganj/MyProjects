defb5d24474cad1071ee0996ac6e00ddff968497
Bishal Adhikary <158996175+bishalraiganj@users.noreply.github.com>
2025-07-06 15:59:06 +0530

    made the ManagedBlocker nested static class and private inside the recursiveTask implementation

--- LogFileAnalyzer/src/main/java/Adhikary/X/monitor/ConcurrentLogMonitor.java
+++ LogFileAnalyzer/src/main/java/Adhikary/X/monitor/ConcurrentLogMonitor.java
@@ -6,7 +6,8 @@ import java.util.Optional;
 import java.util.concurrent.*;
 
 
-class ProperBlockingTask extends RecursiveTask<Optional<LogEntry>> {
+class ProperBlockingTask extends RecursiveTask<Optional<LogEntry>>
+{
 
 
 	@Override
@@ -15,22 +16,45 @@ class ProperBlockingTask extends RecursiveTask<Optional<LogEntry>> {
 
 
 
+
 	}
 
 
 
+	private static class sleepBlocker implements ForkJoinPool.ManagedBlocker {
+		private final long sleepTime;
+
+		private final long startTime;
+
+		/* Since ,
+	we need to make the thread wait at least few seconds for modification to the
+	log file before polling we will check how much time has elapsed from the
+	starting of execution of this task , if enough time elapsed then no need to block
+	which is why we have this startTime field to calculate the elapsed Time
+	*/
+
+		sleepBlocker(long sleepTime, long startTime)
+		{
+			this.sleepTime = sleepTime;
+			this.startTime = System.currentTimeMillis();
+		}
+
+		@Override
+		public boolean block() throws InterruptedException
+		{
+			Thread.sleep(sleepTime);
+			return true;
+		}
+
+		@Override
+		public boolean isReleasable()
+		{
+			return System.currentTimeMillis() - startTime > sleepTime;
+
+		}
+	}
 }
 
-class sleepBlocker extends ForkJoinPool.ManagedBlocker
-{
-	private final long sleepTime;
-
-	private final long startTime;
-
-
-
-
-}
 
 
 public class ConcurrentLogMonitor {
